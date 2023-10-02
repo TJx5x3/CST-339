27e5e32ef6c5545825ef46fa903e448c326cb4fd
Alejandro Barranco-Leyte <alejandrobarranco01@gmail.com>
2023-10-01 21:35:00 -0700

    First commit

---
+++ MilestoneTwo/src/main/java/com/gcu/MilestoneTwoApplication.java
@@ -0,0 +1,13 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestoneTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilestoneTwoApplication.class, args);
+	}
+
+}
